7f073524fc1f14a158066d52433d8676c7eab8cb
Srivathsa PV <pv.srivathsa@gmail.com>
2017-12-19 21:44:24 -0500

    Revert to Notebook Idea for Demo

--- setup.py
+++ setup.py
@@ -1,43 +1,29 @@
 import sys
+import os
 import subprocess
-
-SCRIPT_SRC = './bloby.py'
-SCRIPT_PATH = './bin/bloby'
-
-def _get_python_path():
-	result = subprocess.run(['which', 'python3'], stdout=subprocess.PIPE)
-	return result.stdout.decode('utf-8').replace('\n', '')
 
 def _install_requirements():
 	print('Installing Bloby requirements...')
 	subprocess.run(['pip3', 'install', '-r', 'requirements.txt'])
 
-def _create_bin_script():
-	print('Creating executable...')
-	src = '#!{}\n'.format(_get_python_path())
-	with open(SCRIPT_SRC, 'r') as src_file:
-		src += src_file.read()
+def _create_intern_config():
+	print('Creating intern.cfg...')
+	boss_token = os.environ['BOSS_TOKEN']
+	with open('intern_cfg_tmpl', 'r') as cfg_file:
+		cfg_str = cfg_file.read()
 
-	with open(SCRIPT_PATH, 'w') as out_file:
-		out_file.write(src)
-
-def _chmod_ex_script():
-	print('Setting up permissions...')
-	subprocess.run(['chmod', '+x', SCRIPT_PATH])
+	with open('intern.cfg', 'w') as cfg_file:
+		cfg_file.write(cfg_str.format(boss_token))
 
 def install():
 	_install_requirements()
-	_create_bin_script()
-	_chmod_ex_script()
+	_create_intern_config()
 
 	from colorama import init as color_init
 	from colorama import Fore
 	color_init()
 
 	print(Fore.GREEN + 'Bloby setup successful! Run {} to start the Bloby CLI'.format(SCRIPT_PATH))
-
-def clean():
-	subprocess.run(['rm', './bin/bloby'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
 if __name__ == '__main__':
 	task = sys.argv[1]
