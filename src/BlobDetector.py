0e0254b0cca8f594a185abf6ff3831b4c6f8897c
Srivathsa PV <pv.srivathsa@gmail.com>
2018-04-03 23:02:26 -0400

    Bradley Adaptive Thresholding

--- src/BlobDetector.py
+++ src/BlobDetector.py
@@ -31,6 +31,92 @@ class BlobDetector(object):
         self.data_source = data_source
         self.verbose = verbose
 
+    def _adaptive_thresh_slice(self, img, s=None, t=None):
+
+        # Convert image to numpy array
+        #img = np.array(image).astype(np.float)
+
+        # Default window size is round(cols/8)
+        if s is None:
+            s = np.round(img.shape[1]/8)
+
+        # Default threshold is 15% of the total
+        # area in the window
+        if t is None:
+            t = 50.0
+
+        # Compute integral image
+        intImage = np.cumsum(np.cumsum(img, axis=1), axis=0)
+
+        # Define grid of points
+        (rows,cols) = img.shape[:2]
+        (X,Y) = np.meshgrid(np.arange(cols), np.arange(rows))
+
+        # Make into 1D grid of coordinates for easier access
+        X = X.ravel()
+        Y = Y.ravel()
+
+        # Ensure s is even so that we are able to index into the image
+        # properly
+        s = s + np.mod(s,2)
+
+        # Access the four corners of each neighbourhood
+        x1 = X - s/2
+        x2 = X + s/2
+        y1 = Y - s/2
+        y2 = Y + s/2
+
+        # Ensure no coordinates are out of bounds
+        x1[x1 < 0] = 0
+        x2[x2 >= cols] = cols-1
+        y1[y1 < 0] = 0
+        y2[y2 >= rows] = rows-1
+
+
+        # Count how many pixels are in each neighbourhood
+        count = (x2 - x1) * (y2 - y1)
+
+        # Compute the row and column coordinates to access
+        # each corner of the neighbourhood for the integral image
+        f1_x = x2.astype(int)
+        f1_y = y2.astype(int)
+        f2_x = x2.astype(int)
+        f2_y = (y1 - 1).astype(int)
+        f2_y[f2_y < 0] = 0
+        f3_x = (x1-1).astype(int)
+        f3_x[f3_x < 0] = 0
+        f3_y = y2.astype(int)
+        f4_x = f3_x.astype(int)
+        f4_y = f2_y.astype(int)
+
+
+        # Compute areas of each window
+        sums = intImage[f1_y, f1_x] - intImage[f2_y, f2_x] - intImage[f3_y, f3_x] + intImage[f4_y, f4_x]
+
+        # Compute thresholded image and reshape into a 2D grid
+        out = np.ones(rows*cols, dtype=np.bool)
+        out[img.ravel()*count <= sums*(100.0 - t)/100.0] = False
+
+        # Also convert back to uint8
+        out = 255*np.reshape(out, (rows, cols)).astype(np.uint8)
+        return out
+
+    def _adaptive_thresh(self, img):
+        self.threshold = 7500 #dummy code
+        out_img = np.zeros_like(img, dtype=np.uint16)
+
+        for z in range(img.shape[0]):
+            img_slice = img[z, : ,:]
+            img_slice = img_slice.max() - img_slice
+
+            th_img_slice = self._adaptive_thresh_slice(img_slice)
+            th_img_slice = th_img_slice.max() - th_img_slice
+
+            out_img[z, :, :] = th_img_slice
+
+        return out_img
+
+
     def _gmm_cluster(self, img, data_points, n_components):
         if self.verbose:
             print('Starting GMM Cluster')
@@ -130,10 +216,11 @@ class BlobDetector(object):
         uniq = np.unique(self.img, return_counts=True)
 
         data_points = [p for p in zip(*uniq)]
-        gm_img = self._gmm_cluster(self.img, data_points, self.n_components)
+        #gm_img = self._gmm_cluster(self.img, data_points, self.n_components)
+        gm_img = self._adaptive_thresh(self.img)
 
         eroded_img = gm_img
-        
+
         if self.data_source == 'COLM':
             eroded_img = morphology.erosion(gm_img)
         else:
