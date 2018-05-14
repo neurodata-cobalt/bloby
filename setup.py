c1da411767bd50ab8d12ba7f5f16cb04a98f5c8a
Srivathsa PV <pv.srivathsa@gmail.com>
2018-05-14 01:18:49 -0400

    Folder cleanup

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5',  # Required
+    version='0.20',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -77,11 +77,11 @@ setup(
 
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Srivathsa Pasumarthi',  # Optional
+    author='Srivathsa PV',  # Optional
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='pvsriv@cs.jhu.edu',  # Optional
+    author_email='pv.srivathsa@gmail.com',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
@@ -134,19 +134,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['colorama',
-        'scikit_image',
-        'scipy',
-        'numpy',
-        'requests',
-        'intern',
-        'tifffile',
-        'tqdm',
-        'matplotlib',
-        'progressbar2',
-        'scikit_learn',
-        'pyfiglet'],  # Optional
-
+    #install_requires=['peppercorn'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
@@ -175,6 +163,7 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    #data_files=[('my_data', ['data/data_file'])],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
@@ -183,11 +172,11 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
 
     # List additional URLs that are relevant to your project as a dict.
     #
@@ -199,7 +188,9 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/NeuroDataDesign/bloby/issues',
-        'Funding': 'https://donate.pypi.org'
+        'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/pypa/sampleproject/',
     },
 )
